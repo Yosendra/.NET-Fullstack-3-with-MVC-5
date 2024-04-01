1fc32ed698fc519a03ce08730527be03797e9237
Yosendra <yosendra.evri@gmail.com>
2024-04-02 02:25:56 +0800

    Modularizing JavaScript Code - 04. Applying The DRY Principle

---
+++ 01. Modularizing JavaScript Code/04. Applying The DRY Principle/GigHub/Models/Attendance.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Services.Description;
+
+namespace GigHub.Models
+{
+    public class Attendance
+    {
+        [Key]
+        [Column(Order = 1)]             // this is needed if you use Composite key
+        public int GigId { get; set; }
+        public Gig Gig { get; set; }
+
+        [Key]
+        [Column(Order = 2)]             // this is needed if you use Composite key
+        public string AttendeeId { get; set; }
+        public ApplicationUser Attendee { get; set; }
+
+
+    }
+}
---
+++ 01. Modularizing JavaScript Code/04. Applying The DRY Principle/GigHub/Scripts/app/app.js
@@ -0,0 +1,57 @@
+﻿var GigsController = function () {
+    // we seperate logic for handling event to a function "toggleAttendance"
+    // then we use it when subscribe the click event -> $(".js-toggle-attendance").click(toggleAttendance);)
+
+    // this is the promoted variable, it like field in C# (private variable in a class)
+    var button;
+    var gigId;
+
+    // it is like constructor of the class (let us just assume like that)
+    var init = function () {
+        $(".js-toggle-attendance").click(toggleAttendance);
+    };
+
+    var toggleAttendance = function (e) {
+        /* promote these to module level,
+         * currently it is still local scope for toggleAttendance only
+         * meanwhile we want the button & gigId to be used in the other private function
+         * like done() function because it needs button element inside it
+        */ 
+        //var button = $(e.target);
+        //var gigId = button.data("gig-id");
+
+        button = $(e.target);
+        gigId = button.data("gig-id");
+
+        if (button.hasClass("btn-light")) {
+            $.post("/api/attendances", { gigId })
+                .done(done)
+                .fail(fail)
+        } else {
+            $.ajax({
+                url: "/api/attendances/" + gigId,
+                method: "DELETE",
+            })
+                .done(done)
+                .fail(fail);
+        }
+    };
+
+    var done = function () {
+        var text = button.text() == "Going"
+            ? "Going?"
+            : "Going";
+        button
+            .toggleClass("btn-info")
+            .toggleClass("btn-light")
+            .text(text);
+    };
+
+    var fail = function () {
+        alert("Something failed!");
+    };
+
+    return {
+        init: init
+    }
+}();
