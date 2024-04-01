1c63f0ca3a00480d3969d7487d3dd7d7ff43eb2b
Yosendra <yosendra.evri@gmail.com>
2024-04-02 02:35:30 +0800

    Modularizing JavaScript Code - 05. Better Separation of Concerns

---
+++ 01. Modularizing JavaScript Code/05. Better Separation of Concerns/GigHub/Global.asax.cs
@@ -0,0 +1,34 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * More refactor in toggleAttendance() in app.js
+ * Just look at app.js
+ * 
+ * Look at:
+ * • app.js
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ 01. Modularizing JavaScript Code/05. Better Separation of Concerns/GigHub/Scripts/app/app.js
@@ -0,0 +1,52 @@
+﻿var GigsController = function () {
+    var button;
+    var gigId;
+
+    var init = function () {
+        $(".js-toggle-attendance").click(toggleAttendance);
+    };
+
+    // now toggleAttendance function is more concise
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+        gigId = button.data("gig-id");
+
+        if (button.hasClass("btn-light"))
+            createAttendance();         // refactor
+        else
+            deleteAttendance();         // refactor
+    };
+
+    var createAttendance = function () {
+        $.post("/api/attendances", { gigId })
+            .done(done)
+            .fail(fail)
+    };
+
+    var deleteAttendance = function () {
+        $.ajax({
+            url: "/api/attendances/" + gigId,
+            method: "DELETE",
+        })
+            .done(done)
+            .fail(fail);
+    };
+
+    var done = function () {
+        var text = button.text() == "Going"
+            ? "Going?"
+            : "Going";
+        button
+            .toggleClass("btn-info")
+            .toggleClass("btn-light")
+            .text(text);
+    };
+
+    var fail = function () {
+        alert("Something failed!");
+    };
+
+    return {
+        init: init
+    }
+}();
