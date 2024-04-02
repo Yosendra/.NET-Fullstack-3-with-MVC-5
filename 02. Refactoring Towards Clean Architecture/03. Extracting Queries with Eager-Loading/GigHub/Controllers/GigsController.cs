d5fdf0bea7ef9d53a42a807b6883405fe51ce390
Yosendra <yosendra.evri@gmail.com>
2024-04-02 10:48:50 +0800

    Refactoring Towards Clean Architecture - 03. Extracting Queries with Eager-Loading

---
+++ 02. Refactoring Towards Clean Architecture/03. Extracting Queries with Eager-Loading/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.Repositories;
+using GigHub.ViewModels.Gigs;
+using GigHub.ViewModels.Shared;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly GigRespository _gigRespository;
+        private readonly AttendanceRepository _attendanceRepository;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+            _gigRespository = new GigRespository(_context);
+            _attendanceRepository = new AttendanceRepository(_context);
+        }
+
+        [HttpPost]
+        public ActionResult Search(GigsVM viewModel)
+        {
+            return RedirectToAction("Index", "Home", new { query = viewModel.SearchTerm });
+        }
+
+        public ActionResult Details(int id)
+        {
+            var gig = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .SingleOrDefault(g => g.Id == id);
+
+            if (gig == null)
+                return HttpNotFound();
+
+            var viewModel = new GigDetailsVM { Gig = gig, };
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+
+                viewModel.IsAttending = _context.Attendances
+                    .Any(a => a.GigId == gig.Id && a.AttendeeId == userId);
+
+                viewModel.IsFollowing = _context.Followings
+                    .Any(f => f.FolloweeId == gig.ArtistId && f.FollowerId == userId);
+            }
+
+            return View("Details", viewModel);
+        }
+
+        public ActionResult Mine()
+        {
+            var artistId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => 
+                    g.ArtistId == artistId &&
+                    g.DateTime > DateTime.Now &&
+                    g.IsCanceled == false)
+                .Include(g => g.Genre)
+                .ToList();
+
+            return View(gigs);
+        }
+
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var viewModel = new GigsVM
+            {
+                UpcomingGigs = _gigRespository.GetGigsUserAttending(userId),
+                IsShowActions = User.Identity.IsAuthenticated,
+                Heading = "Gigs I'm Going",
+                Attendances = _attendanceRepository.GetFutureAttendances(userId).ToLookup(a => a.GigId),
+            };
+
+            return View("Gigs", viewModel);
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormVM
+            {
+                Genres = _context.Genres.ToList(),
+                Heading = "Add a Gig",
+            };
+
+            return View("GigForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GigFormVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue,
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            var viewModel = new GigFormVM
+            {
+                Id = gig.Id,
+                Genres = _context.Genres.ToList(),
+                Genre = gig.GenreId,
+                Date = gig.DateTime.ToString("d MMM yyyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Venue = gig.Venue,
+                Heading = "Edit a Gig",
+            };
+
+            return View("GigForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(GigFormVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            //var userId = User.Identity.GetUserId();
+            //var gig = _context.Gigs
+            //    .Include(g => g.Attendances.Select(a => a.Attendee))
+            //    .Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+            var gig = _gigRespository.GetGigWithAttendees(viewModel.Id);
+
+            // because we remove the condition for ArtistId in GigRespository.GetGigWithAttendees()
+            // we put the check here
+            if (gig == null)
+                return HttpNotFound();
+            if (gig.ArtistId != User.Identity.GetUserId())
+                return new HttpUnauthorizedResult();
+
+            gig.Modify(viewModel.GetDateTime(),
+                viewModel.Venue,
+                viewModel.Genre);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+    }
+}
---
+++ 02. Refactoring Towards Clean Architecture/03. Extracting Queries with Eager-Loading/GigHub/Global.asax.cs
@@ -0,0 +1,35 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * There is query using eager-loading at GigsController.Details()
+ * Just look at the code, quite complex
+ * 
+ * Look at:
+ * • GigsController.cs -> Edit()
+ * • GigRespository.cs -> GetGigWithAttendees()
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
