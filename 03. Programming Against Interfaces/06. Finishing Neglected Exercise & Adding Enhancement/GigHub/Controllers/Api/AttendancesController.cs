98a0021f5a620ffcd852fd1ae7b8aacdee89d657
Yosendra <yosendra.evri@gmail.com>
2024-04-03 04:44:58 +0800

    Finishing Neglected Exercise & Adding Enhancement (Cont.)

---
+++ 03. Programming Against Interfaces/06. Finishing Neglected Exercise & Adding Enhancement/GigHub/Controllers/Api/AttendancesController.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GigHub.Core.Models;
+using Microsoft.AspNet.Identity;
+using GigHub.Persistence;
+using GigHub.Core.Dtos;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] AttendanceDto dto)
+        {
+            var gigId = dto.GigId;
+            var userId = User.Identity.GetUserId();
+            var isAttendanceExist = _context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId);
+            
+            if (isAttendanceExist)
+                return BadRequest("The attendance already exists.");
+
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = userId,
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteAttendance(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var attendance = _context.Attendances.SingleOrDefault(a => a.GigId == id && a.AttendeeId == userId);
+
+            if (attendance == null)
+                return NotFound();
+
+            _context.Attendances.Remove(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
