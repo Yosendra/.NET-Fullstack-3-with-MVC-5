f47aa4a7a6b1cc8734381ca91c28331562c5a092
Yosendra <yosendra.evri@gmail.com>
2024-04-03 16:53:47 +0800

    Finishing Neglected Exercise & Adding Enhancement (Refactor GigsController-API)

--- 03. Programming Against Interfaces/06. Finishing Neglected Exercise & Adding Enhancement/GigHub/Controllers/Api/GigsController.cs
+++ 03. Programming Against Interfaces/06. Finishing Neglected Exercise & Adding Enhancement/GigHub/Controllers/Api/GigsController.cs
@@ -1,36 +1,32 @@
-﻿using System.Data.Entity;
-using System.Linq;
-using System.Web.Http;
-using GigHub.Core.Models;
+﻿using System.Web.Http;
+using GigHub.Core;
 using Microsoft.AspNet.Identity;
-using GigHub.Persistence;
 
 namespace GigHub.Controllers.Api
 {
     [Authorize]
     public class GigsController : ApiController
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public GigsController()
+        public GigsController(IUnitOfWork unitOfWork)
         {
-            _context = new ApplicationDbContext();
+            _unitOfWork = unitOfWork;
         }
 
         [HttpDelete]
         public IHttpActionResult Cancel(int id)
         {
-            var userId = User.Identity.GetUserId();
-            var gig = _context.Gigs
-                .Include(g => g.Attendances.Select(a => a.Attendee))
-                .Single(g => g.Id == id && g.ArtistId == userId);
-
-            if (gig.IsCanceled)
+            var gig = _unitOfWork.Gigs.GetGigWithAttendees(id);
+            if (gig == null || gig.IsCanceled)
                 return NotFound();
 
-            gig.Cancel();
+            var userId = User.Identity.GetUserId();
+            if (gig.ArtistId != userId)
+                return Unauthorized();
 
-            _context.SaveChanges();
+            gig.Cancel();
+            _unitOfWork.Complete();
 
             return Ok();
         }
