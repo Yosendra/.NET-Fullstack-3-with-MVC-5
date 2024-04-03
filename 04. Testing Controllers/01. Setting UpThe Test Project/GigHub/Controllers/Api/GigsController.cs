1b4633da8504cb37b48d7e3eede284ee02b791fb
Yosendra <yosendra.evri@gmail.com>
2024-04-03 23:12:30 +0800

    Testing Controllers - 01. Setting UpThe Test Project

---
+++ 04. Testing Controllers/01. Setting UpThe Test Project/GigHub/Controllers/Api/GigsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using GigHub.Core;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GigsController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var gig = _unitOfWork.Gigs.GetGigWithAttendees(id);
+            if (gig == null || gig.IsCanceled)
+                return NotFound();
+
+            var userId = User.Identity.GetUserId();
+            if (gig.ArtistId != userId)
+                return Unauthorized();
+
+            gig.Cancel();
+            _unitOfWork.Complete();
+
+            return Ok();
+        }
+    }
+}
---
+++ 04. Testing Controllers/01. Setting UpThe Test Project/GigHub/Global.asax.cs
@@ -0,0 +1,38 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * Create Test Project. 
+ * Right click on solution, Add new project -> choose Unit Test Project (.Net Framework) 
+ * 
+ * Install new package on Unit Test Project:
+ * • Moq                (4.2.1510.2205)
+ * • FluentAssertions   (3.3.0)
+ * 
+ * Look at:
+ * • 
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
