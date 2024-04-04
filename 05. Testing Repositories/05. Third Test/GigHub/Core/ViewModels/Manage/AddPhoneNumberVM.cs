1df5dd8063280240cd694d6894af7f929ff906c5
Yosendra <yosendra.evri@gmail.com>
2024-04-04 17:36:42 +0800

    Testing Repositories - 05. Third Test

---
+++ 05. Testing Repositories/05. Third Test/GigHub/Core/ViewModels/Manage/AddPhoneNumberVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Core.ViewModels.Manage
+{
+    public class AddPhoneNumberVM
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string Number { get; set; }
+    }
+}
