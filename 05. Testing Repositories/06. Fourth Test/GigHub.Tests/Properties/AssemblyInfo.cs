41d8faba93bc0c14b072a8b29f4b7220634924a2
Yosendra <yosendra.evri@gmail.com>
2024-04-04 17:42:40 +0800

    Testing Repositories - 06. Fourth Test

---
+++ 05. Testing Repositories/06. Fourth Test/GigHub.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GigHub.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("958e1b31-3eb9-4c8f-bf25-f4ae8e5b7c6c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 05. Testing Repositories/06. Fourth Test/GigHub/Global.asax.cs
@@ -0,0 +1,34 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * https://learn.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
+ * Create extension methods class "MockDbSetExtensions" under Extensions folder
+ * 
+ * Look at:
+ * • GigRepositoryTests.cs -> GetUpcomingGigsByArtist_GigIsForTheGivenArtistAndIsInTheFuture_ShouldBeReturned()
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
